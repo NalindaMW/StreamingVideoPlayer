6bf14b5226dc7e5be669749dc8ebd8ff9e931ca2
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-06-24 00:32:38 +0530

    Initial commit

---
+++ StreamingVideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreamingVideoPlayer
+//
+//  Created by uwickna on 2022-06-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
